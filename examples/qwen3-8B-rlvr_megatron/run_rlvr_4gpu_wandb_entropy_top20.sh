76b5db7b73f2ee4d014c92576f5d6060b3e0de5c
kellysun <1433806072@qq.com>
2026-04-25 03:23:52 +0800

    wandb offline

--- examples/qwen3-8B-rlvr_megatron/run_rlvr_4gpu_wandb_entropy_top20.sh
+++ examples/qwen3-8B-rlvr_megatron/run_rlvr_4gpu_wandb_entropy_top20.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set +x
+export WANDB_MODE=offline
 
 CONFIG_PATH=$(basename $(dirname $0))
-python examples/start_rlvr_pipeline.py --config_path $CONFIG_PATH --config_name rlvr_config_4gpu_wandb_entropy_top20
+PYTHONPATH="$(pwd):${PYTHONPATH}" python examples/start_rlvr_pipeline.py --config_path $CONFIG_PATH --config_name rlvr_config_4gpu_wandb_entropy_top20
